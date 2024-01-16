cbc9513535c6fdef95b11310703aac548e2ab7c9
PrairieSnpr <midnryanmiller@gmail.com>
2024-01-15 20:54:13 -0500

    Switching to relays, out working

--- constants.py
+++ constants.py
@@ -3,19 +3,34 @@ from secrets import UNIQE_ID_PRE
 LED_ENABLE = True
 LED_PIN = "LED"
 
-# Out Pins
-ST_UP_PIN = 4
-ST_DN_PIN = 5
-ST_CL_PIN = 9
-ST_ON_PIN = 10
+# Out Pins (7)
+# Start Top left, DN-Right-UP
+ST_ON_PIN = 5  # Bake
+#ST_BR_PIN = 3  # Broil
+#ST_TM_PIN = 4  # Timer
+#ST_CK_PIN = 5  # Clock
+ST_CL_PIN = 2  # CLR/OFF
+ST_DN_PIN = 3  # DWN
+ST_UP_PIN = 4  # UP
 
+# Think 2 is clear
 # In Pins
-ST_UP_C_PIN = 14
-ST_DN_C_PIN = 15
-ST_CL_C_PIN = 19
-ST_ON_C_PIN = 20
+# Not bothering to monitor the other inputs, just don't see a point
+ST_UP_C_PIN = 9  # Temp Up
+ST_DN_C_PIN = 10  # Temp Dwn
+ST_CL_C_PIN = 11  # Clr/Off
+ST_ON_C_PIN = 12  # Bake
+ST_BR_C_PIN = 13  # Broil
 
-DEB_TIME = 500
+# LED In Pins
+ST_ON_LED_PIN = 14
+ST_BL_LED_PIN = 15
+
+# Actual Temp, haven't even looked at it.
+ST_T_IN_PIN = 27
+
+
+DEB_TIME = 10
 
 MIN_TEMP = 170
 MAX_TMP = 500
@@ -26,10 +41,12 @@ TMP_STEP = 5
 DIR_DWN = "D"
 DIR_UP = "U"
 
-BTN_DELAY = 100
+BTN_DELAY = 100  # ms
+
+CUR_TMP_RD_PER = 5000
 
 AV_MSG = "online"
-AV_PERIOD = 15000
+AV_PERIOD = 15000  # ms
 
 
 MQTT_PREFIX = "isilentllc"
@@ -78,8 +95,9 @@ CFG_MSG = {
     "precision": TMP_P,
     "mode_stat_t": "~/status/mode",
     "mode_cmd_t": "~/command/mode",
-    "temp_stat_t": "~/status/temp",
-    "temp_cmd_t": "~/command/temp",
+    "temp_stat_t": "~/status/set_temp",
+    "temp_cmd_t": "~/command/set_temp",
+    "curr_temp_t": "~/status/cur_temp",
     "avty_t": "~/status/availability",
     "modes": AV_MODES,
     "dev": CFG_DEV,
@@ -87,7 +105,13 @@ CFG_MSG = {
 
 
 MD_CMD_TPC = CMD_TPC + "/mode"
-TMP_CMD_TPC = CMD_TPC + "/temp"
+TMP_CMD_TPC = CMD_TPC + "/set_temp"
 
 MD_ST_TPC = ST_TPC + "/mode"
-TMP_ST_TPC = ST_TPC + "/temp"
+TMP_ST_TPC = ST_TPC + "/set_temp"
+TMP_CUR_TPC = ST_TPC + "/cur_temp"
+
+
+# Temp sensor
+BETA = 3950  # Verify
+KELVIN_C = 273.15  # Verify
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import time
 import machine
 
 from machine import Timer, Pin
+from machine import ADC
+
+from math import log
 
 from secrets import WIFI_AP, WIFI_PWD, MQTT_HOST
 
@@ -29,7 +32,9 @@ from constants import (
     TMP_STEP,
     DIR_DWN,
     DIR_UP,
-    BTN_DELAY
+    BTN_DELAY,
+    TMP_CUR_TPC,
+    CUR_TMP_RD_PER,
 )
 
 from constants import (
@@ -41,7 +46,10 @@ from constants import (
     ST_CL_C_PIN,
     ST_DN_C_PIN,
     ST_UP_C_PIN,
+    ST_T_IN_PIN,
 )
+
+from constants import KELVIN_C, BETA
 
 # Local configuration
 config["ssid"] = WIFI_AP
@@ -49,13 +57,29 @@ config["wifi_pw"] = WIFI_PWD
 config["server"] = MQTT_HOST
 
 cur_mode = MODE_OFF
-cur_temp = START_TMP
+cur_set_temp = START_TMP
+cur_act_temp = MIN_TEMP  # Need something?
 debounce_time = 0
 
-oven_on = Pin(ST_ON_PIN, Pin.OUT, Pin.PULL_DOWN)
-oven_clear_off = Pin(ST_CL_PIN, Pin.OUT, Pin.PULL_DOWN)
-oven_temp_up = Pin(ST_UP_PIN, Pin.OUT, Pin.PULL_DOWN)
-oven_temp_down = Pin(ST_DN_PIN, Pin.OUT, Pin.PULL_DOWN)
+
+oven_on_btn = Pin(ST_ON_PIN, Pin.OUT)
+oven_on_btn.off()
+oven_clear_off_btn = Pin(ST_CL_PIN, Pin.OUT)
+oven_clear_off_btn.off()
+oven_temp_up_btn = Pin(ST_UP_PIN, Pin.OUT)  # is oven on
+oven_temp_up_btn.off()
+oven_temp_down_btn = Pin(ST_DN_PIN, Pin.OUT)
+oven_temp_down_btn.off()
+
+
+
+async def toggle_btn(pin):
+    btn = Pin(pin, Pin.OUT)
+    btn.on()
+    await asyncio.sleep_ms(BTN_DELAY)
+    btn.off()
+    await asyncio.sleep_ms(BTN_DELAY)
+
 
 async def wait_pin_change(pin):
     # wait for pin to change value
@@ -69,83 +93,109 @@ async def wait_pin_change(pin):
             active = 0
         await asyncio.sleep_ms(1)
 
-async def set_off():
-    oven_clear_off.off()
-    oven_clear_off.on()
-    await asyncio.sleep_ms(BTN_DELAY)
-    oven_clear_off.off()
 
-async def set_on():
-    oven_on.off()
-    oven_on.on()
-    await asyncio.sleep_ms(BTN_DELAY)
-    oven_on.off()
+async def set_on(complete=False):
+    await toggle_btn(ST_ON_PIN)
+    await temp_up()
+    if complete:
+        await toggle_btn(ST_ON_PIN)
+
+
+        
+
+
+async def set_off():
+    await toggle_btn(ST_CL_PIN)
+
+
+async def temp_up():
+    await toggle_btn(ST_UP_PIN)
+
+
+async def temp_dn():
+    await toggle_btn(ST_DN_PIN)
 
 
 async def set_temp(set_point):
     global cur_mode
+    global cur_set_temp
+    print(f"Cur Mode: {cur_mode}, Cur Set Pnt: {cur_set_temp} Set to: {set_point}")
     direction = None
 
     if set_point > MAX_TMP:
         set_point = MAX_TMP
+        print("Dir Up")
 
     if set_point < MIN_TEMP:
         set_point = MIN_TEMP
+        
+    steps = 0
 
-    if set_point == START_TMP:
-        pass
+    if set_point == cur_set_temp:
+        steps = 0
     elif set_point < START_TMP:
-        steps = int((START_TMP - set_point) / TMP_STEP)
+        steps = int((cur_set_temp - set_point) / TMP_STEP)
         direction = DIR_DWN
+        print("Dir Dwn: %s Steps" % steps)
     elif set_point > START_TMP:
-        steps = int((set_point - START_TMP) / TMP_STEP)
+        steps = int((set_point - cur_set_temp) / TMP_STEP)
         direction = DIR_UP
+        print("Dir Up: %s Steps" % steps)
+    
+    steps = abs(steps)
 
     if cur_mode == MODE_OFF:
-        await set_on()
+        await set_on(complete=False)
+        cur_mode = MODE_HEAT
+        
 
     for i in range(steps):
         if direction == DIR_UP:
-            oven_temp_up.on()
-            await asyncio.sleep_ms(BTN_DELAY)
-            oven_temp_up.off()
+            await temp_up()
         if direction == DIR_DWN:
-            oven_temp_down.on()
-            await asyncio.sleep_ms(BTN_DELAY)
-            oven_temp_down.off()
+            await temp_dn()
 
     if cur_mode == MODE_OFF:
-        await set_on()
-        cur_mode = MODE_HEAT
+        await set_on(complete=False)
+
+    cur_set_temp = set_point
+
 
 async def received_command(command, mtch_topic, client):
     global cur_mode
-    global cur_temp
+    global cur_set_temp
 
     if mtch_topic == TMP_CMD_TPC:
         print("Set Temp to %s" % command)
         new_temp = int(float(command))
         if MIN_TEMP < new_temp < MAX_TMP:
-            cur_temp = new_temp
-            await set_temp(cur_temp)
-            await update_temp_state(client)
+            print("Setting Temp to %s" % new_temp)
+            await set_temp(new_temp)
+            await update_temp_set_state(client)
             await update_mode_state(client)
-            print("Set Temp to %s" % cur_temp)
+            print("Set Temp to %s" % cur_set_temp)
     elif mtch_topic == MD_CMD_TPC:
         if command in AV_MODES and command != cur_mode:
-            cur_mode = command
-            if cur_mode == MODE_OFF:
+            #cur_mode = command
+            if command == MODE_OFF:
                 await set_off()
-            if cur_mode == MODE_HEAT:
-                await set_on()
-                cur_temp = START_TMP
-                await update_temp_state(client)
+                cur_set_temp = START_TMP
+                cur_mode = MODE_OFF
+            if command == MODE_HEAT:
+                await set_on(complete=True)
+                cur_set_temp = START_TMP
+                cur_mode = MODE_HEAT
+            await update_temp_set_state(client)
             await update_mode_state(client)
             print("Set Mode to %s" % cur_mode)
 
 
-async def update_temp_state(client):
-    await client.publish(TMP_ST_TPC.encode("utf_8"), str(cur_temp).encode("utf_8"))
+async def update_temp_cur_state(client):
+    await client.publish(TMP_CUR_TPC.encode("utf_8"), str(cur_act_temp).encode("utf_8"))
+
+
+async def update_temp_set_state(client):
+    await client.publish(TMP_ST_TPC.encode("utf_8"), str(cur_set_temp).encode("utf_8"))
 
 
 async def update_mode_state(client):
@@ -163,41 +213,62 @@ async def read_on_button(client):
     global cur_mode
     oven_on_ctl = Pin(ST_ON_C_PIN, Pin.IN, Pin.PULL_UP)
     while True:
+        print("Wait for BK pin")
         await wait_pin_change(oven_on_ctl)
+        print("Pin BK")
         cur_mode = MODE_HEAT
         await update_mode_state(client)
 
 
 async def read_clr_button(client):
     global cur_mode
-    global cur_temp
+    global cur_set_temp
     oven_clear_off_ctl = Pin(ST_CL_C_PIN, Pin.IN, Pin.PULL_UP)
     while True:
+        print("Wait for CLR pin")
         await wait_pin_change(oven_clear_off_ctl)
-        cur_temp = START_TMP
+        print("Pin CLR")
+        cur_set_temp = START_TMP
         cur_mode = MODE_OFF
-        await update_temp_state(client)
+        await update_temp_set_state(client)
         await update_mode_state(client)
 
 
 async def read_up_button(client):
-    global cur_temp
+    global cur_set_temp
     oven_temp_up_ctl = Pin(ST_UP_C_PIN, Pin.IN, Pin.PULL_UP)
     while True:
+        print("Wait for UP pin")
         await wait_pin_change(oven_temp_up_ctl)
-        if cur_temp < MAX_TMP:
-            cur_temp += TMP_STEP
-            await update_temp_state(client)
+        print("Pin UP")
+        if cur_set_temp < MAX_TMP:
+            cur_set_temp += TMP_STEP
+            await update_temp_set_state(client)
 
 
 async def read_dwn_button(client):
-    global cur_temp
+    global cur_set_temp
     oven_temp_down_ctl = Pin(ST_DN_C_PIN, Pin.IN, Pin.PULL_UP)
     while True:
+        print("Wait for DWN pin")
         await wait_pin_change(oven_temp_down_ctl)
-        if cur_temp > MIN_TEMP:
-            cur_temp -= TMP_STEP
-            await update_temp_state(client)
+        print("Pin DWN")
+        if cur_set_temp > MIN_TEMP:
+            cur_set_temp -= TMP_STEP
+            await update_temp_set_state(client)
+
+
+async def read_cur_tmp(client):
+    global cur_act_temp
+    oven_current_act_temp = ADC(ST_T_IN_PIN)
+    while True:
+        temp_val = oven_current_act_temp.read_u16()
+
+        cur_act_temp = round(
+            (1 / (log(1 / (65535 / temp_val - 1)) / BETA + 1 / 298.15) - KELVIN_C), 1
+        )
+        await update_temp_cur_state(client)
+        await asyncio.sleep_ms(CUR_TMP_RD_PER)
 
 
 async def messages(client):  # Respond to incoming messages
@@ -214,13 +285,17 @@ async def up(client):  # Respond to connectivity being (re)established
         await client.up.wait()  # Wait on an Event
         client.up.clear()
         await client.subscribe(SUB_TPC, 1)  # renew subscriptions
-        await send_config(client)
-        await update_mode_state(client)
-        await update_temp_state(client)
-        asyncio.create_task(read_up_button(client))
-        asyncio.create_task(read_dwn_button(client))
-        asyncio.create_task(read_on_button(client))
-        asyncio.create_task(read_clr_button(client))
+        await send_config(client) # Send the discovery msg
+        await update_mode_state(client) # Send the current mode
+        await update_temp_set_state(client) # Send the current temp
+
+        #  Will there be any issues here, do we need to cancel existing and restart them?
+        # Moved to the main function, for now, still not certain where it should be
+        # asyncio.create_task(read_up_button(client))
+        # asyncio.create_task(read_dwn_button(client))
+        # asyncio.create_task(read_on_button(client))
+        # asyncio.create_task(read_clr_button(client))
+        # asyncio.create_task(read_cur_tmp(client))
 
 
 async def main(client):
@@ -228,6 +303,11 @@ async def main(client):
     for coroutine in (
         up,
         messages,
+        read_up_button,
+        read_dwn_button,
+        read_on_button,
+        read_clr_button,
+        read_cur_tmp,
     ):
         asyncio.create_task(coroutine(client))
     while True:
@@ -239,11 +319,13 @@ async def main(client):
 config["queue_len"] = 1  # Use event interface with default queue size
 MQTTClient.DEBUG = True  # Optional: print diagnostic messages
 
-try: 
+try:
     client = MQTTClient(config)
 except OSError:
     machine.reset()
 try:
     asyncio.run(main(client))
+except OSError:
+    machine.reset()
 finally:
     client.close()  # Prevent LmacRxBlk:1 errors
